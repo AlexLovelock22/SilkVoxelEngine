5820ae155d53bc73e0fb9738ab09acaaf4fe4d4a
Alex Lovelock <93345315+AlexLovelock22@users.noreply.github.com>
2026-02-28 17:25:18 +0000

    biome ruffles

--- obj/Debug/net9.0/VoxelEngine-Silk.Net-1.0.AssemblyInfo.cs
+++ obj/Debug/net9.0/VoxelEngine-Silk.Net-1.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoxelEngine-Silk.Net-1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc04dfeb6c5b0e990f98d134cef98c4c912bb0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7be2e11db4c4386f4d06516c8d2fa7cfd2f9e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoxelEngine-Silk.Net-1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoxelEngine-Silk.Net-1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
